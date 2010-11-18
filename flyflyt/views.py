333535f5add00dc177014f903c0dad9757ea7267
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2010-11-18 20:16:50 +0100

    Refactoring: split long methods into smaller ones.

--- flyflyt/views.py
+++ flyflyt/views.py
@@ -1,6 +1,6 @@
 import logging
 from flyflyt import app
-from flask import render_template, make_response
+from flask import render_template
 from flightinfo.airport import AirPort
 from flightinfo.flightinformationservice import FlightInformationService
 from flightinfo.airportparser import AirPortParser
@@ -23,64 +23,82 @@ def datetimeformat(value, format='%H:%M'):
 
 app.jinja_env.filters['datetimeformat'] = datetimeformat
 
-def generate_airport_html():
+AIRPORT_XML_CACHE_TIMEOUT = 60 * 60 * 24
+AIRLINE_XML_CACHE_TIMEOUT = 60 * 60 * 24
+STATUS_XML_CACHE_TIMEOUT = 60 * 60 * 24
+FLIGHT_XML_CACHE_TIMEOUT = 60
+AIRPORT_HTML_CACHE_TIMEOUT = 60 * 60 * 24
+FLIGHT_HTML_CACHE_TIMEOUT = 60 
+
+def generate_airport_factory():
     airports_xml = memcache.get("airports_xml")
     if airports_xml is None:
         airports_xml = FlightInformationService.download_airport_xml()
-        memcache.set("airports_xml", airports_xml, 6000)
-        
-    airports = AirPortParser.parse_airports(airports_xml)
-    airport_factory = AirPortFactory(airports)
-
-    airports = airport_factory.get_norwegian_airports()
-    airports_html = render_template('list_airports.html', airports=airports)
-
-    return airports_html
-
-
-def generate_flight_html(code):
-    airports_xml = memcache.get("airports_xml")
-    if airports_xml is None:
-        airports_xml = FlightInformationService.download_airport_xml()
-        memcache.set("airports_xml", airports_xml, 6000)
+        memcache.set("airports_xml", airports_xml, AIRPORT_XML_CACHE_TIMEOUT)
 
     airports = AirPortParser.parse_airports(airports_xml)
     airport_factory = AirPortFactory(airports)
-    
+
+    return airport_factory
+
+def generate_airline_factory():
     airlines_xml = memcache.get("airlines_xml")
     if airlines_xml is None:
         airlines_xml = FlightInformationService.download_airline_xml()
-        memcache.set("airlines_xml", airlines_xml, 6000)
+        memcache.set("airlines_xml", airlines_xml, AIRLINE_XML_CACHE_TIMEOUT)
 
     airlines = AirlineParser.parse_airlines(airlines_xml)
     airline_factory = AirlineFactory(airlines)
-    
+    return airline_factory
+
+def generate_status_factory():
     status_xml = memcache.get("status_xml")
     if status_xml is None:
         status_xml = FlightInformationService.download_flight_status_xml()
-        memcache.set("status_xml", status_xml, 6000)
+        memcache.set("status_xml", status_xml, STATUS_XML_CACHE_TIMEOUT)
 
     statuses = FlightStatusParser.parse_statuses(status_xml)
     status_factory = FlightStatusFactory(statuses)
+    return status_factory
 
 
-    flights_xml_name = code + "_xml"
+def get_flights(airport, airline_factory, airport_factory, status_factory):
+    flights_xml_name = airport.code + "_xml"
     xml = memcache.get(flights_xml_name)
-    airport = airport_factory.get_airport_by_code(code)
     if xml is None:
         query = Query(airport)
         xml = FlightInformationService.download_flight_xml(query)
-        memcache.set(flights_xml_name, xml, 60)
-    
-    flights = FlightParser.parse_flights(xml, airline_factory, airport_factory,
-                                         status_factory)
+        memcache.set(flights_xml_name, xml, FLIGHT_XML_CACHE_TIMEOUT)
+
+    return FlightParser.parse_flights(xml, airline_factory, airport_factory,
+                                      status_factory)
 
 
-    def is_departure(flight):
-        return flight.direction == Flight.Directions.DEPARTURE
+def generate_airport_html():
+    airport_factory = generate_airport_factory()
+    airports = airport_factory.get_norwegian_airports()
+    airports_html = render_template('list_airports.html', airports=airports)
+    return airports_html
 
-    def is_arrival(flight):
-        return flight.direction == Flight.Directions.ARRIVAL
+
+
+def is_departure(flight):
+    return flight.direction == Flight.Directions.DEPARTURE
+
+def is_arrival(flight):
+    return flight.direction == Flight.Directions.ARRIVAL
+
+
+
+def generate_flight_html(code):
+
+    airport_factory = generate_airport_factory()
+    airline_factory = generate_airline_factory()
+    status_factory = generate_status_factory()
+
+    airport = airport_factory.get_airport_by_code(code)    
+    flights = get_flights(airport, airline_factory, 
+                          airport_factory, status_factory)
 
     departures = filter(is_departure, flights)
     arrivals = filter(is_arrival, flights)
@@ -95,10 +113,10 @@ def generate_flight_html(code):
 
 @app.route('/')
 def list_airports():
-    airports_html = memcache.get("list_airports_html")
+    airports_html = memcache.get("airports_html")
     if airports_html is None:
         airports_html = generate_airport_html()
-        memcache.set("list_airports_html", airports_html, 6000)
+        memcache.set("airports_html", airports_html, AIRPORT_HTML_CACHE_TIMEOUT)
     else:
         logging.info("Found airport list in memcache.")
 
@@ -106,13 +124,13 @@ def list_airports():
 
 @app.route('/airport/<code>')
 def list_flights(code):
-    airport_html_id = "airport_html" + code
+    airport_html_id = "airport_html_" + code
     flight_html = memcache.get(airport_html_id)
     if flight_html is None:
         flight_html = generate_flight_html(code)
-        memcache.set(airport_html_id, flight_html, 60)
+        memcache.set(airport_html_id, flight_html, FLIGHT_HTML_CACHE_TIMEOUT)
     else:
-        logging.info("Found html for " + code + " in memcache.")
+        logging.info("Found html for airport " + code + " in memcache.")
         
     return flight_html
 
