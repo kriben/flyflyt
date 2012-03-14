cd15843b3a4b3a4d008ad06cdee9a50f96dcb2e9
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2012-03-14 20:05:18 +0100

    Remove unused settings.py (with unused secrets).

--- flyflyt/__init__.py
+++ flyflyt/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
-import settings
 
 app = Flask('flyflyt')
-app.config.from_object('flyflyt.settings')
 
 import views
--- flyflyt/settings.py
+++
@@ -1,4 +0,0 @@
-DEBUG=False
-SECRET_KEY='dev_key_h8dflkxccds'
-CSRF_ENABLED=True
-CSRF_SESSION_LKEY='dev_key_h8gfhSadf=f'
